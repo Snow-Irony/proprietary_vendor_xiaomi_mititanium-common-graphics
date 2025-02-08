5b70e450acbb676293e99019c948bcbef59b8e3f
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-02-08 09:10:42 +0000

    mititanium-common: Update project structure & Convert libGLESv2_adreno.so and libC2D2.so to build module

--- LA.UM.10.6.2.r1-01600-89xx.0/extract-vendor.mk
+++ LA.UM.10.6.2.r1-01600-89xx.0/extract-vendor.mk
@@ -6,6 +6,49 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0
 
 PRODUCT_COPY_FILES += \
-    
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/system_ext/lib64/libgralloc.qti.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libgralloc.qti.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/system_ext/lib64/libqdMetaData.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libqdMetaData.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/bin/mm-pp-dpps:$(TARGET_COPY_OUT_VENDOR)/bin/mm-pp-dpps \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/bin/hw/vendor.display.color@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.display.color@1.0-service \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/etc/init/vendor.display.color@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.display.color@1.0-service.rc \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/eglSubDriverAndroid.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/libEGL_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libEGL_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/libGLESv2_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv2_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/libq3dtools_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libq3dtools_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/egl/libq3dtools_esx.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libq3dtools_esx.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libC2D2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libC2D2.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libCB.so:$(TARGET_COPY_OUT_VENDOR)/lib/libCB.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libOpenCL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOpenCL.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libadreno_utils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libadreno_utils.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libc2d30_bltlib.so:$(TARGET_COPY_OUT_VENDOR)/lib/libc2d30_bltlib.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libgsl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgsl.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib/libllvm-glnext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libllvm-glnext.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/eglSubDriverAndroid.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/eglSubDriverAndroid.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/libEGL_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/libEGL_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/libGLESv1_CM_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/libGLESv1_CM_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/libGLESv2_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/libGLESv2_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/libq3dtools_adreno.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/libq3dtools_adreno.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/egl/libq3dtools_esx.so:$(TARGET_COPY_OUT_VENDOR)/lib64/egl/libq3dtools_esx.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libC2D2.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libC2D2.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libCB.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libCB.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libOpenCL.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libOpenCL.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libadreno_utils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libadreno_utils.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libc2d30_bltlib.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc2d30_bltlib.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libdisp-aba.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdisp-aba.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libgsl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgsl.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libhdr_tm.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhdr_tm.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libllvm-glnext.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libllvm-glnext.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libscalar.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libscalar.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libsdedrm.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsdedrm.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libsdm-color.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsdm-color.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libsdm-diag.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsdm-diag.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libsdm-disp-vndapis.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsdm-disp-vndapis.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libsdmextension.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsdmextension.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/libtinyxml2_1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtinyxml2_1.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/vendor.display.color@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.display.color@1.0.so \
+    vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/proprietary/vendor/lib64/vendor.display.postproc@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.display.postproc@1.0.so 
 
 PRODUCT_PACKAGES += \
+    libGLESv2_adreno \
+    libC2D2
