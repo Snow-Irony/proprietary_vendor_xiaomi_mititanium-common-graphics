15bc0bfea2d1ace33f081e4af9b8675eaee461af
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-02-09 12:43:01 +0000

    mititanium-common: drop daisy graphics

--- mititanium-common-graphics-vendor.mk
+++ mititanium-common-graphics-vendor.mk
@@ -1,5 +1,1 @@
-ifeq ($(TARGET_USES_Q_DISPLAY_STACK),true)
-    $(call inherit-product, vendor/xiaomi/mititanium-common-graphics/daisy-Q/extract-vendor.mk)
-else
-    $(call inherit-product, vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/extract-vendor.mk)
-endif
+$(call inherit-product, vendor/xiaomi/mititanium-common-graphics/LA.UM.10.6.2.r1-01600-89xx.0/extract-vendor.mk)
